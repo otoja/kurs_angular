a9089626a47dc883c2c3e4625025c572259272c1
Kamila Przychodzeń <kamila.przychodzen@gmail.com>
2016-11-06 14:27:28 +0100

    modal config

--- app/controllers/CourseCtr.js
+++ app/controllers/CourseCtr.js
@@ -1,4 +1,21 @@
-app.controller('CourseController', function($scope){
+app.controller('CourseController', function ($scope, $uibModal) {
     $scope.test = 'Hello Kama ;)';
+    $scope.animationsEnabled = true;
+
+    var modalConfig = {
+        animation: $scope.animationsEnabled,
+        ariaLabelledBy: 'modal-title-top',
+        ariaDescribedBy: 'modal-body-top',
+        templateUrl: './app/templates/modal.html',
+        size: 'sm',
+        controller: function ($scope, username) {
+            $scope.name = username;
+        },
+        resolve:{ username: function () { return $scope.test; } }
+    };
+
+    $scope.openModal = function () {
+        $uibModal.open(modalConfig);
+    };
 });
 
--- app/scripts.js
+++ app/scripts.js
@@ -1,3 +1,3 @@
 'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.bootstrap.modal','ui.bootstrap.tpls']);
